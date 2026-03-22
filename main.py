854aca18fc49f642faa8b4b1e271096780909f05
Erasyl570 <118047591+Erasyl570@users.noreply.github.com>
2026-03-22 13:30:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import threading
 from telegram_bot import run_bot
+from web import app
+import os
+
+def start_bot():
+    run_bot()
 
 if __name__ == "__main__":
-    run_bot()
+    threading.Thread(target=start_bot).start()
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
