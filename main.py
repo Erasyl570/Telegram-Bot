7601985c8171fdb3b3d17af6686052213bfeda12
Erasyl570 <118047591+Erasyl570@users.noreply.github.com>
2026-03-22 13:52:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import threading
+import os
+from flask import Flask
 from telegram_bot import run_bot
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def start_bot():
     run_bot()
+
+if __name__ == "__main__":
+    
+    # запускаем бота в отдельном потоке
+    threading.Thread(target=start_bot).start()
+
+    # открываем порт для Render
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
